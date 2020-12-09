216252ad3360901a59f7552d922ebf1032b9748b
Fatima <fatima.majid95@gmail.com>
2020-12-09 18:06:00 +0530

    Update cricsheet.py

--- cricsheet.py
+++ cricsheet.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd 
 import numpy as np 
+import plotly
+plotly.offline.init_notebook_mode(connected=True)
+import plotly.offline as py
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go 
