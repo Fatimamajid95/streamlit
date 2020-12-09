e13ece03c4c49cc47ab4d0dc9de0cc0d6fe4a299
Fatima <fatima.majid95@gmail.com>
2020-12-09 20:06:03 +0530

    Update cricsheet.py

--- cricsheet.py
+++ cricsheet.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go 
 import matplotlib.pyplot as plt 
 import io
 import seaborn as sns
